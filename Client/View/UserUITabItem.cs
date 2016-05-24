3e84cd681b89ec6665d9c1662188f4a7018bd349
Dmitriy Yushko <rafiek57@gmail.com>
2016-05-24 21:43:25 +0300

    Remove unused imports from "Client/View/UserUITabItem.cs".

--- Client/View/UserUITabItem.cs
+++ Client/View/UserUITabItem.cs
@@ -1,21 +1,16 @@
 ﻿using System.Windows.Controls;
-using Client.Manager;
 
 namespace Client.View
 {
     class UserUITabItem : TabItem
     {
-        private RoomManager manager;
-
         public UserUITabItem(string title, bool isClosed)
         {
             var tab = new UserTabItem(title, isClosed);
-            manager = RoomManager.GetInstance();
             tab.OnClose += (sender, e) => 
             {
                 var tabControl = GetParentTabControl();
                 tabControl?.Items?.Remove(this);
-                manager?.Remove(title);
             };
             Header = tab;
         }
