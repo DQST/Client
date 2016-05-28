9f7ca3ef8555730e4f36849a2dd1356cedca50f4
Dmitriy Yushko <rafiek57@gmail.com>
2016-05-29 01:51:44 +0300

    Add new control "MProgressBar" in project.

---
+++ Client/View/MProgressBar.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Client.View
+{
+    /// <summary>
+    /// Логика взаимодействия для MProgressBar.xaml
+    /// </summary>
+    public partial class MProgressBar : UserControl
+    {
+        public MProgressBar()
+        {
+            InitializeComponent();
+        }
+
+        public MProgressBar(long max) : this()
+        {
+            bar.Maximum = max;
+        }
+
+        public void SetValue(int v)
+        {
+            bar.Value = v;
+        }
+    }
+}
