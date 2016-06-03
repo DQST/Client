7528b0609bc0a043126b5ac3b0f2d3eb122d9d0b
Dmitriy Yushko <rafiek57@gmail.com>
2016-06-04 02:02:50 +0300

    Add login window in project.

---
+++ Client/View/Login.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using HelpLib.Wrapper;
+using HelpLib.Config;
+using Client.Extensions;
+
+namespace Client.View
+{
+    /// <summary>
+    /// Логика взаимодействия для Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        private OloService service;
+
+        public Login()
+        {
+            InitializeComponent();
+            service = new OloService(this);
+            Network.OnReceive += (s, e) =>
+            {
+                var data = OloProtocol.Encode(e.Buffer.ToStr());
+                service.Parse(data, e.RemoteEndPoint);
+            };
+            enterButton.Click += (s, e) =>
+            {
+                if (!string.IsNullOrWhiteSpace(login.Text) && !string.IsNullOrWhiteSpace(pass.Password))
+                {
+                    var olo = OloProtocol.GetOlo("login", login.Text, pass.Password);
+                    Network.Send(olo.ToBytes(), Config.GlobalConfig.RemoteHost);
+                }
+            };
+        }
+
+        [OloField(Name = "enter")]
+        public void Enter(params object[] args)
+        {
+            Close();
+        }
+
+        [OloField(Name = "error")]
+        public void Error(params object[] args)
+        {
+            MessageBox.Show(args[0].ToString(), "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+}
