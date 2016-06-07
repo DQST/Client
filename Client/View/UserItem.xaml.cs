e7f4f5acfe905cccf95c717974db2f131c30809e
Dmitriy Yushko <rafiek57@gmail.com>
2016-06-08 02:31:55 +0300

    Add "UserItem" in project

---
+++ Client/View/UserItem.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Windows.Controls;
+
+namespace Client.View
+{
+    /// <summary>
+    /// Логика взаимодействия для UserItem.xaml
+    /// </summary>
+    public partial class UserItem : UserControl
+    {
+        public UserItem()
+        {
+            InitializeComponent();
+        }
+
+        public void PushMessage(object obj)
+        {
+            var pos = mesageList.Items.Add(obj);
+            mesageList.SelectedIndex = pos;
+            mesageList.ScrollIntoView(mesageList.SelectedItem);
+            mesageList.SelectedIndex = -1;
+        }
+
+        public void SetUsers(IEnumerable en)
+        {
+            usersList.Items.Clear();
+            foreach (var item in en)
+                usersList.Items.Add(item);
+        }
+    }
+}
